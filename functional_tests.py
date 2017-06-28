3d4913a5abe85b2d8f9df4c977a1db7df26537e8
Daniel Gitu <gitudaniel@gmail.com>
2017-06-28 10:35:30 +0300

    Solved error messages from failing functional and unit tests

--- functional_tests.py
+++ functional_tests.py
@@ -35,10 +35,11 @@ class NewVisitorTest(unittest.TestCase):
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
-        inputbox.send_keys(Keys.ENTER)
-
-        table = self.browser.find_element_by_id('id_list_table')
+        table = self.browser.find_element_by_id("id_list_table")
         rows = table.find_elements_by_tag_name('tr')
+        inputbox.send_keys(Keys.ENTER) # re-ordered arrangement of inputbox to solve 
+                                       # Unable to locate element: [id="id_list_table"] error
+
         self.assertTrue(
                 any(row.text == '1: Buy peacock feathers' for row in rows),
                 "New to-do item did not appear in table"
--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,5 @@
+import re
+
 from django.template.loader import render_to_string
 from django.core.urlresolvers import resolve
 from django.test import TestCase
@@ -11,8 +13,36 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
+    def remove_csrf(self, html_code):
+        csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+        return re.sub(csrf_regex, '', html_code)
+
     def test_home_page_returns_correct_html(self):
+        """render_to_string ignores csrf_token in template
+        Django's render_to_string ignores the csrf_token in the template
+        To resolve this we pass the request as an argument in expected_html
+        A new csrf request is generated with every request.
+        We therefore have 2 different csrf middleware tokens generated
+        To take care of this we need to remove the token
+        We define a function remove_csrf that substitutes the line of text
+        containing the csrf middleware token with an empty string.
+
+        Refer to:https://stackoverflow.com/questions/34629261/django-render-to-string-ignores-csrf-token
+        """
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('lists/home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        expected_html = render_to_string('lists/home.html', request=request)
+        view_html = response.content.decode()
+        self.assertEqual(
+                self.remove_csrf(view_html), 
+                self.remove_csrf(expected_html)
+        )
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertIn('A new list item', response.content.decode())
