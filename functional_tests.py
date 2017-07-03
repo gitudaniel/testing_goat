57e154eba9d49842ac163f822fbeadebb82e6c40
Daniel Gitu <gitudaniel@gmail.com>
2017-07-03 08:02:15 +0300

    Extended our functional and unit tests to check for template variables

--- functional_tests.py
+++ functional_tests.py
@@ -6,7 +6,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
+        self.browser.implicitly_wait(10)
 
     def tearDown(self):
         self.browser.quit()
@@ -35,15 +35,12 @@ class NewVisitorTest(unittest.TestCase):
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name('tr')
         inputbox.send_keys(Keys.ENTER) # re-ordered arrangement of inputbox to solve 
                                        # Unable to locate element: [id="id_list_table"] error
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name('tr')
 
-        self.assertTrue(
-                any(row.text == '1: Buy peacock feathers' for row in rows),
-                "New to-do item did not appear in table"
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly" (Edith is very
--- lists/tests.py
+++ lists/tests.py
@@ -14,10 +14,6 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
     def remove_csrf(self, html_code):
-        csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
-        return re.sub(csrf_regex, '', html_code)
-
-    def test_home_page_returns_correct_html(self):
         """render_to_string ignores csrf_token in template
         Django's render_to_string ignores the csrf_token in the template
         To resolve this we pass the request as an argument in expected_html
@@ -29,6 +25,11 @@ class HomePageTest(TestCase):
 
         Refer to:https://stackoverflow.com/questions/34629261/django-render-to-string-ignores-csrf-token
         """
+
+        csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+        return re.sub(csrf_regex, '', html_code)
+
+    def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
         expected_html = render_to_string('lists/home.html', request=request)
@@ -46,3 +47,12 @@ class HomePageTest(TestCase):
         response = home_page(request)
 
         self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string('lists/home.html',
+                {'new_item_text': 'A new list item'},
+                request=request)
+        view_html = response.content.decode()
+        self.assertEqual(
+                self.remove_csrf(view_html),
+                self.remove_csrf(expected_html)
+        )
+
