13058f53939f40f96599a14a26bc86ecdff9e2e9
gitudaniel <gitudaniel@gmail.com>
2017-08-01 09:21:10 +0300

    Added unique urls for each list

--- lists/urls.py
+++ lists/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home_page, name='home_page'),
     url(r'^lists/new$', views.new_list, name='new_list'),
-    url(r'^the-only-list-in-the-world/$', views.view_list, name='view_list'),
+    url(r'^lists/(.+)/$', views.view_list, name='view_list'),
 ]
