c382f12ce8dc6ed55bf3f22235968a6878f2a4de
gitudaniel <gitudaniel@gmail.com>
2017-09-14 07:23:30 +0300

    Use new form in home_page, simplify tests. NB it breaks stuff

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -8,6 +8,7 @@ from django.utils.html import escape
 
 from lists.views import home_page
 from lists.models import Item, List
+from lists.forms import ItemForm
 
 
 class HomePageTest(TestCase):
@@ -17,32 +18,34 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
 
-    def remove_csrf(self, html_code):
-        """render_to_string ignores csrf_token in template
-        Django's render_to_string ignores the csrf_token in the template
-        To resolve this we pass the request as an argument in expected_html
-        A new csrf request is generated with every request.
-        We therefore have 2 different csrf middleware tokens generated
-        To take care of this we need to remove the token
-        We define a function remove_csrf that substitutes the line of text
-        containing the csrf middleware token with an empty string.
+    #def remove_csrf(self, html_code):
+    #    """render_to_string ignores csrf_token in template
+    #    Django's render_to_string ignores the csrf_token in the template
+    #    To resolve this we pass the request as an argument in expected_html
+    #    A new csrf request is generated with every request.
+    #    We therefore have 2 different csrf middleware tokens generated
+    #    To take care of this we need to remove the token
+    #    We define a function remove_csrf that substitutes the line of text
+    #    containing the csrf middleware token with an empty string.
 
-        Refer to:https://stackoverflow.com/questions/34629261/django-render-to-string-ignores-csrf-token
-        """
+    #    Refer to:https://stackoverflow.com/questions/34629261/django-render-to-string-ignores-csrf-token
+    #    """
 
-        csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
-        return re.sub(csrf_regex, '', html_code)
+    #    csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+    #    return re.sub(csrf_regex, '', html_code)
 
 
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        expected_html = render_to_string('lists/home.html', request=request)
-        view_html = response.content.decode()
-        self.assertEqual(
-                self.remove_csrf(view_html), 
-                self.remove_csrf(expected_html)
-        )
+    #def test_home_page_returns_correct_html(self):
+    #    request = HttpRequest()
+    #    response = home_page(request)
+    #    expected_html = render_to_string('lists/home.html', request=request)
+    #    view_html = response.content.decode()
+    #    # print(view_html)
+    #    print(expected_html)
+    #    self.assertEqual(
+    #            self.remove_csrf(view_html), 
+    #            self.remove_csrf(expected_html)
+    #    )
 
 
     def test_validation_errors_are_sent_back_to_home_page_template(self):
@@ -57,6 +60,14 @@ class HomePageTest(TestCase):
         self.client.post('/lists/new', data={'item_text': ''})
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
+
+
+    def test_home_page_uses_item_form(self):
+        """
+        Test that ModelForms are being used.
+        """
+        response = self.client.get('/')
+        self.assertIsInstance(response.context['form'], ItemForm)
 
 
 
--- lists/views.py
+++ lists/views.py
@@ -2,11 +2,12 @@ from django.core.exceptions import ValidationError
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 
+from lists.forms import ItemForm
 from lists.models import Item, List
 
 
 def home_page(request):
-    return render(request, 'lists/home.html')
+    return render(request, 'lists/home.html', {'form': ItemForm()})
 
 
 def view_list(request, list_id):
