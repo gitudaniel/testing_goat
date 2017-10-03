e0380b67817e2b409040029813531dac0fbf5c9e
gitudaniel <gitudaniel@gmail.com>
2017-10-03 08:59:40 +0300

    Added some Javascript and QUnit tests

---
+++ static/lists/list.js
@@ -0,0 +1,12 @@
+//var initialize = function () {
+//    $('input[name="text"]').on('keypress', function () {
+//        $('.has-error').hide();
+//    });
+//};
+
+
+var initialize = function () {
+    $('input[name="text"]').on('keypress', function () {
+        $('.has-error').hide();
+    });
+};
