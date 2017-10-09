64b28ea9fcb5f269de5e356faae872ac444c8958
gitudaniel <gitudaniel@gmail.com>
2017-10-09 08:10:28 +0300

    Added passwordless login via email with logging and tests

--- superlists/settings.py
+++ superlists/settings.py
@@ -30,6 +30,23 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Logging using the logging package from the python standard library
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
 
 # Application definition
 
@@ -41,7 +58,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists',
+    'accounts',
 ]
+
+AUTH_USER_MODEL = 'accounts.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
