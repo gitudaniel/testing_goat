486c01387900a11a33dd59f1c0a53d0133149545
gitudaniel <gitudaniel@gmail.com>
2017-10-11 14:07:02 +0300

    Wired our views to use passwordless login

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -9,11 +9,6 @@ from accounts.models import Token
 
 class SendLoginEmailViewTest(TestCase):
     
-    def test_redirects_to_home_page(self):
-        response = self.client.get('/accounts/login?token=abcd123')
-        self.assertRedirects(response, '/')
-
-
     @patch('accounts.views.send_mail')
     def test_sends_mail_to_address_from_post(self, mock_send_mail):
         self.client.post('/accounts/send_login_email', data={
@@ -71,3 +66,35 @@ class SendLoginEmailViewTest(TestCase):
         expected_url = f'http://testserver/accounts/login?token={token.uid}'
         (subject, body, from_email, to_list), kwargs = mock_send_mail.call_args
         self.assertIn(expected_url, body)
+
+
+
+
+@patch('accounts.views.auth')
+class LoginViewTest(TestCase):
+
+    def test_redirects_to_home_page(self, mock_auth):
+        response = self.client.get('/accounts/login?token=abcd123')
+        self.assertRedirects(response, '/')
+
+
+    def test_calls_authenticate_with_uid_from_get_request(self, mock_auth):
+        self.client.get('/accounts/login?token=abcd123')
+        self.assertEqual(
+            mock_auth.authenticate.call_args,
+            call(uid='abcd123')
+        )
+
+
+    def test_calls_auth_login_with_user_if_there_is_one(self, mock_auth):
+        response = self.client.get('/accounts/login?token=abcd123')
+        self.assertEqual(
+            mock_auth.login.call_args,
+            call(response.wsgi_request, mock_auth.authenticate.return_value)
+        )
+
+
+    def test_does_not_login_if_user_is_not_authenticated(self, mock_auth):
+        mock_auth.authenticate.return_value = None
+        self.client.get('/accounts/login?token=abcd123')
+        self.assertEqual(mock_auth.login.called, False)
